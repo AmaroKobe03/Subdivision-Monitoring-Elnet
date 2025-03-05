4295d98ca7a35de103f8d0f53bf57b0d107f66eb
AmaroKobe03 <148180353+AmaroKobe03@users.noreply.github.com>
2025-03-05 11:20:56 +0800

    updates

--- ElnetSubdivi/Models/FacilityViewModel.cs
+++ ElnetSubdivi/Models/FacilityViewModel.cs
@@ -6,8 +6,6 @@
     public string ButtonText { get; set; }
     public string DTime { get; set; }
     public string Type { get; set; }
-    public string Status { get; set; }
-    public Dictionary<string, string> Icons { get; set; }
 
     // New property to check if the card will display two buttons
     public bool ShowEditCancelButtons { get; set; }
@@ -19,9 +17,7 @@
         FacilityName = string.Empty;
         DTime = string.Empty;
         Type = string.Empty;
-        Status = string.Empty;
         ActionName = string.Empty;
         ButtonText = string.Empty;
-        Icons = new Dictionary<string, string>();
     }
 }
