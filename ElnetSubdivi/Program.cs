b11a135d1295d6df49eda8c38f7cd32f92d7e3e9
AmaroKobe03 <148180353+AmaroKobe03@users.noreply.github.com>
2025-03-09 01:13:09 +0800

    User Management Update

--- ElnetSubdivi/Program.cs
+++ ElnetSubdivi/Program.cs
@@ -1,15 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// ✅ Move Authentication Setup Here:
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Home/Landing";
+    });
+
+var app = builder.Build();  
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,6 +26,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // ✅ Make sure to use authentication middleware
 app.UseAuthorization();
 
 app.MapControllerRoute(
